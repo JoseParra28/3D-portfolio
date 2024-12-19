de0f71dd6935afef3e32c95065feac1f2ba8b2f5
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-19 16:10:29 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Test from "./components/Test"
+
+const  App = () => {
+
+  return (
+    <>
+    <Test/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Test.jsx
@@ -0,0 +1,7 @@
+
+
+function Test (){
+    return <h1>Hello There</h1>
+}
+
+export default Test
