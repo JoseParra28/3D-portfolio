e74289e319bfbaedc32b49724c14061c6ea284db
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-21 16:44:37 +0000

    react practice

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,13 +1,29 @@
 import { Canvas } from "@react-three/fiber"
+import { h1, input } from "framer-motion/client"
 
 
 const Test = () => {
+    let nameN = 'Jose';
+    let count = 0;
+    const handleClick = (name) => {
+    if(count < 3 ){
+        count++;
+        console.log(`${name} you clicked me ${count} times`)
+    } else {
+        console.log(`${name} stop clicking!!`)
+    };
+    }
+    const styles = {
+            backgroundColor: "hsl(200,100%, 50%)",
+            color: "white",
+            padding: "10px 20px",
+            borderRadius: "5px",
+            border: "none",
+            cursor: "pointer",   
+    }
+
     return (
-        <Canvas>
-            <mesh>
-                <boxGeometry/>
-            </mesh>
-        </Canvas>
+        <button onClick={() => handleClick(nameN)} style={styles}>Click me</button>
     )
 }
 
