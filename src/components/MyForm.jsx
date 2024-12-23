044f39c4657030d886978616662a7b0e7bd3c635
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-23 10:45:34 +0000

    react

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -4,15 +4,28 @@ import React, {useState} from "react"
 const Myform = () => {
 
     const [ name, setName] = useState("Guest")
+    const [payment, setPayment] = useState("")
 
     function handleName(event){
         setName(event.target.value)
+    }
+
+    function paymentHandeler (event){
+        setPayment(event.target.value)
     }
 
     return(
         <div>
             <input value={name} onChange={handleName} type="text" />
             <p>Name: {name}</p>
+
+        <select onChange={paymentHandeler} value="Payment" id="">
+            <option value="">Select an option</option>
+            <option value="Visa">Visa</option>
+            <option value="Mastercard">Mastercard</option>
+            <option value="Revolut">Revolut</option>
+        </select>
+        <p>Payment: {payment}</p>
         </div>
     )
 }
