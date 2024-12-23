a3a6b740f10f156dfa1130d26780e4c0ee51a991
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-23 04:44:30 +0000

    react

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -7,6 +7,7 @@ const Test = () => {
 
     const [ name, setName] = useState("Guest")
     const [age, setAge] = useState(0)
+    const [isEmployed, setIsEmployed] = useState(false)
 
     const updateName = () => {
         setName("Jose")
@@ -14,14 +15,25 @@ const Test = () => {
 
     const incrementAge = () => {
         setAge(age + 1)
+        if(age >= 5 ){
+            setName("You are over aged")
+        }
     }
+
+    const toggleEmployStatus = () => {
+        setIsEmployed(! isEmployed)
+    }
+
     return (
         <div>
-       <p>Name: {name} </p>
+       <p className="bold">Name: {name} </p>
        <button onClick={updateName} className="btn btn-warning">Set name</button>
 
        <p>Name: {age} </p>
        <button onClick={incrementAge} className="btn btn-warning">Set age</button>
+
+       <p>Is Emplyed: {isEmployed ? "Yes" : "No"} </p>
+       <button onClick={toggleEmployStatus} className="btn btn-warning">Toggle status</button>
         </div>
     )
 }
