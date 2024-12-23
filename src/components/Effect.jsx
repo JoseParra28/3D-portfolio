d72a9f1b1a892f5242a7ba036882d944062ea2f9
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-23 20:55:19 +0000

    react

--- src/components/Effect.jsx
+++ src/components/Effect.jsx
@@ -4,19 +4,31 @@ import React, {useEffect, useState} from "react"
 const Effect = () => {
 
     const [count, setCount] = useState(0)
+    const [color, setColor] = useState("green")
 
     useEffect(() => {
         document.title = `Count: ${count}`
-    })
+    }, [count])
 
     function AddCount () {
         setCount(c => c + 1)
     }
 
+    function RemoveCount () {
+        setCount(c => c - 1)
+    }
+
+    function changeColor () {
+        setColor(c => c === "green" ? "red" : "green")
+    }
+
     return (
         <div>
-            <p>Count: {count}</p>
+            <p style={{color: color}}>Count: {count}</p>
             <button className="btn " onClick={AddCount}>Add</button>
+            <button className="btn " onClick={RemoveCount}>Remove</button><br />
+
+            <button onClick={changeColor}>Change color</button>
         </div>
       
     )
